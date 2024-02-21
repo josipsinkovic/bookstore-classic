b832f6137ce759e585197dffdfd7266b9ddf3c12
Josip Sinković <josip.sinkovic06@gmail.com>
2024-02-21 14:51:12 +0100

    feat: separate book creation logic, add sections to index.php, and apply styling

---
+++ public_html/includes/book.php
@@ -0,0 +1,4 @@
+<img src="" alt="">
+<p class="book-author"></p>
+<h4 class="book-name"></h4>
+<p class="book-price"></p>
--- public_html/index.php
+++ public_html/index.php
@@ -9,7 +9,10 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/assets/css/base.css">
+    <link rel="stylesheet" href="/assets/css/main_page.css">
     <script src="/assets/js/fetch_books.js"></script>
+    <script src="/assets/js/carousel.js"></script>
+    <script src="/assets/js/quotes.js"></script>
     <script src="https://kit.fontawesome.com/8bbbed924d.js" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -17,23 +20,219 @@ session_start();
         <?php require_once('includes/header.php'); ?>
     </header>
     <main>
-        <div id="4" class="books">
-            <img src="" alt="">
-            <h4 class="book-name"></h4>
-            <p class="book-author"></p>
-            <p class="book-price"></p>
-        </div>
-        <div id="1" class="books">
-            <img src="" alt="">
-            <h4 class="book-name"></h4>
-            <p class="book-author"></p>
-            <p class="book-price"></p>
-        </div>
-        <div id="3" class="books">
-            <img src="" alt="">
-            <h4 class="book-name"></h4>
-            <p class="book-author"></p>
-            <p class="book-price"></p>
+        <div class="container">
+            <section class="banner">
+                <div class="buttons">
+                    <i class="fa-solid fa-chevron-left fa-xl left"></i>
+                    <i class="fa-solid fa-chevron-right fa-xl right"></i>
+                </div>
+                <div class="carousel">
+                    <div class="carousel-item active">
+                        <a href="">
+                            <img src="/assets/images/banners/images.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="carousel-item">
+                        <a href="">
+                            <img src="" alt="">
+                        </a>
+                    </div>
+                    <div class="carousel-item">
+                        <a href="">
+                            <img src="" alt="">
+                        </a>
+                    </div>
+                    <div class="carousel-item">
+                        <a href="">
+                            <img src="" alt="">
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <section class="new-products">
+                <div class="buttons">
+                    <i class="fa-solid fa-chevron-left fa-xl left" id="new-product-left"></i>
+                    <i class="fa-solid fa-chevron-right fa-xl right" id="new-product-right"></i>
+                </div>
+                <div class="new-products-header">
+                    <h1>Novi proizvodi</h1>
+                    <div class="show-all">
+                        <a href="">Prikaži sve</a>
+                    </div>
+                </div>
+                <div class="carousel">
+                    <div class="carousel-item active">
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="discounts">
+                <div class="discounts-header">
+                    <h1>Proizvodi na akciji</h1>
+                    <div class="show-all">
+                        <a href="">Prikaži sve</a>
+                    </div>
+                </div>
+                <div class="carousel">
+                    <div class="carousel-item active">
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="quote">
+                <div class="quote-header">
+                    <h1>Citat dana</h1>
+                </div>
+                <div class="quote-content">
+                    <div class="quote-text">
+                        <span></span>
+                    </div>
+                    <div class="quote-person"></div>
+                </div>
+            </section>
+            <section class="croatian-literature">
+                <div class="buttons">
+                    <i class="fa-solid fa-chevron-left fa-xl left" id="croatian-literature-left"></i>
+                    <i class="fa-solid fa-chevron-right fa-xl right" id="croatian-literature-right"></i>
+                </div>
+                <div class="croatian-literature-header">
+                    <h1>Hrvatski klasici</h1>
+                    <div class="show-all">
+                        <a href="">Prikaži sve</a>
+                    </div>
+                </div>
+                <div class="carousel">
+                    <div class="carousel-item active">
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                        <div class="book">
+                            <?php require("includes/book.php"); ?>
+                        </div>
+                    </div>
+            </section>
         </div>
     </main>
     <footer>
